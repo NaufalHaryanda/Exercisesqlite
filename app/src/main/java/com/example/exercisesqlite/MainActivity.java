a063d0eabd1b8e50d41895ba71e5962f00f873d0
NaufalHaryanda <naufalharyanda10@gmail.com>
2020-04-20 21:33:28 +0700

    Initial commit

---
+++ app/src/main/java/com/example/exercisesqlite/MainActivity.java
@@ -0,0 +1,64 @@
+package com.example.exercisesqlite;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    public final static String EXTRA_MESSAGE = "MESSAGE";
+    private ListView listView;
+    DBHelper mydb;
+    private FloatingActionButton klik;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        klik = findViewById(R.id.fab);
+        klik.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                pindah();
+            }
+        });
+
+
+        //Database SQLite
+        mydb = new DBHelper(this);
+        ArrayList arrayList = mydb.getAllContacts();
+        ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, arrayList);
+
+        listView = (ListView) findViewById(R.id.LV);
+        registerForContextMenu(listView);
+        listView.setAdapter(arrayAdapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
+                int id_To_Search = arg2 + 1;
+
+                Bundle dataBundle = new Bundle();
+                dataBundle.putInt("id", id_To_Search);
+
+                Intent intent = new Intent(getApplicationContext(), tambah_data.class);
+                intent.putExtras(dataBundle);
+                startActivity(intent);
+            }
+        });
+    }
+
+    public void pindah() {
+        Intent intent = new Intent(MainActivity.this, tambah_data.class);
+        startActivity(intent);
+    }
+}
---
+++ app/src/main/java/com/example/exercisesqlite/tambah_data.java
@@ -0,0 +1,6 @@
+package com.example.exercisesqlite;
+
+import android.app.Activity;
+
+public class tambah_data extends Activity {
+}
